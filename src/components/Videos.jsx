bd62f28dce479a4950e2d96d47e8142c6950c816
Shagun Gauttam <gauttamshagun12@gmail.com>
2023-05-02 23:16:07 +0530

    second commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Stack, Box } from "@mui/material";
+
+import {  VideoCard,Loader } from "./";
+
+const Videos = ({videos, direction}) => {
+  if(!videos?.length) return <Loader />;
+    return (
+        <Stack direction={direction || "row"}  flexWrap="wrap" justifyContent="start" gap={2}>
+          {videos.map((item, idx) => (
+            <Box key={idx}>
+              {item.id.videoId && <VideoCard video={item} /> }
+            </Box>
+          ))}
+        </Stack>
+  );
+};
+
+export default Videos;
